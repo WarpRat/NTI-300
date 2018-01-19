34fb0a44ee5f0452ce956b58ac818b0fc7c777b1
Robert Russell <23439640+WarpRat@users.noreply.github.com>
2018-01-19 15:50:20 -0800

    Added a message about the website being under construction so visitors know to check back soon!

--- apache.sh
+++ apache.sh
@@ -9,6 +9,6 @@ systemctl restart httpd #Start apache now
 
 sed -i 's/^/#/g' /etc/httpd/conf.d/welcome.conf #Comment out the default welcome page
 
-echo -e "<html>\n<h1>This is my fun new web page! Hooray!!</h1>\n<html>" > /var/www/html/index.html  #write a new landing page for the website
+echo -e "<html>\n<h1>This is my fun new web page! Hooray!!</h1>\n<br><br>\n<h2>This page is under construction, please check back soon!</h2><br>\n</html>" > /var/www/html/index.html  #write a new landing page for the website
 
 
