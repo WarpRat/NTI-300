ed301095aa3ddade195be09905bed1a70c5b5ac6
Robert Russell <23439640+WarpRat@users.noreply.github.com>
2018-01-28 15:23:47 -0800

    Adding real script

--- nti300bootstrap.sh
+++ nti300bootstrap.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
+#Make a directory for the scripts to pull down
 mkdir /root/bin
-echo WAAASSSSUPPP > /root/bin/test.txt
-for i in {0..5}; do
-	touch /root/bin/touch_test$i
+
+#Pull the script that will perform initial updates and install Apache with the basic site, make it executable, and run it.
+curl https://raw.githubusercontent.com/WarpRat/NTI-300/master/apache.sh > /root/bin/startup.sh
+chmod +x /root/bin/startup.sh
+/root/bin/startup.sh
+
+#Pull the script that will look at disk use, make it executable, and pull down a cron tab with the script set to run each minute.
+curl https://raw.githubusercontent.com/WarpRat/NTI-300/master/du_to_html.sh > /root/bin/du_to_html.sh
+chmod +x /root/bin/du_to_html.sh
+curl https://raw.githubusercontent.com/WarpRat/NTI-300/master/robert > /var/spool/cron/root
+
+#Create some dummy data to make things more interesting
+for i in melanie andy john max; do
+	count=${RANDOM:0:3}
+	mkdir /home/$i
+	cd /home/$i
+	dd if=/dev/zero of=/home/$i/testfile.txt bs=1M count=$count
 done
