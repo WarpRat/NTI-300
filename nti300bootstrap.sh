f442b3e26f8256d069ddbc96cae3f32f9f5ca982
Robert Russell <23439640+WarpRat@users.noreply.github.com>
2018-02-16 00:24:08 -0800

    updates

--- nti300bootstrap.sh
+++ nti300bootstrap.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 yum install -y epel-release
-yum update
+yum update -y
 yum install -y python-pip
 pip install virtualenv
 pip install --upgrade pip
-yum install python34 python-pip
+yum install -y python34 python-pip
 
-useradd -m -p pac8bc4CkyUqU robert
+#useradd -m -p pac8bc4CkyUqU robert
+
+useradd -r -s /sbin/nologin django
+useradd -m -G wheel django robert
+passwd -d robert
+passwd -e robert
+
 
 cd /opt
 
@@ -19,5 +25,8 @@ source bin/activate
 pip install django
 django-admin startproject project1
 
-chown -R robert ../
+chown -R django ../
+chmod -R g=u ../
+
+
 
