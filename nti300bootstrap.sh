61a6d336aa32f5c9bcc4a5a2b59e57e9986293e1
Robert Russell <23439640+WarpRat@users.noreply.github.com>
2018-02-17 00:04:42 -0800

    final improvements for first django bootstrap script. Added authorized_keys fetching

--- nti300bootstrap.sh
+++ nti300bootstrap.sh
@@ -14,14 +14,32 @@ pip install virtualenv
 pip install --upgrade pip
 yum install -y python34 python-pip
 
+#Set variables here:
+new_user=robert
+auth_key_dir="http://keys.melanieclark.info"
+
+#Get the public IP of the EC2.
+cur_ip=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+
+
+
 #Create a service account for django to run under, don't allow login.
 #Create an account for myself and force creating a password on first use.
 #Also make sure I'm in the correct group.
 useradd -r -s /sbin/nologin django
-useradd -m -G wheel,django robert
-passwd -d robert
-passwd -e robert
+useradd -m -G wheel,django "$new_user"
+passwd -d "$new_user"
+passwd -e "$new_user"
 
+#
+if cd /home/"$new_user"/.ssh/ || mkdir /home/"$new_user"/.ssh && cd /home/"$new_user"/.ssh; then
+	curl "$auth_key_dir" > authorized_keys
+	chown robert:robert authorized_keys
+	chmod 600 authorized_keys
+else
+	echo "Error creating .ssh directory and authorized keys file for $new_user - was the user created?" |\
+	       	tee /home/$new_user/ssh_error.log |\
+	       	logger -t [NEW_USER] -p error
 
 cd /opt
 
@@ -39,9 +57,6 @@ django-admin startproject project1
 #Change owernship of all the django files to the django service user. Set group and owner permissions to be the same.
 chown -R django /opt/django/
 chmod -R g=u /opt/django/
-
-#Get the public IP of the EC2.
-cur_ip=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
 
 #Add the public IP to the allowed hosts file.
 sed -i "s/ALLOWED_HOSTS \= \[/&'$cur_ip'/" /opt/django/project1/project1/settings.py
