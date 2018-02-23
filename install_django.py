31f5b1549fa8b9dab6b169d3398319a74702ec7f
Robert Russell <23439640+WarpRat@users.noreply.github.com>
2018-02-22 20:43:07 -0800

    Python version of the django install script (whoops)

--- install_django.py
+++ install_django.py
@@ -27,3 +27,15 @@ def start_django():
              'source /opt/django/bin/activate &&' + \
              '/opt/django/project1/manage.py runserver 0:8000 &')
     
+
+print('Setting up the install')
+input('Press any key to continue')
+setup_install()
+
+print('Installing Django')
+input('Press any key to continue')
+install_django()
+
+print('Starting the server')
+input('Press any key to continue')
+start_django()
