2b39d31bafabe6eb40b3cab09fe073d755cab755
Robert Russell <23439640+WarpRat@users.noreply.github.com>
2018-02-22 21:02:06 -0800

    ready for first test

--- install_django.py
+++ install_django.py
@@ -3,15 +3,15 @@
 import os
 
 def setup_install():
-    
+
     os.system('yum install -y epel-release')
     os.system('yum update -y')
     os.system('yum install -y python34 python-pip')
     os.system('pip install --upgrade pip')
-    os.system('pip install django')
-    
+    os.system('pip install virtualenv')
+
 def install_django():
-    
+
     os.chdir('/opt')
     os.system('virtualenv -p python3 django')
     os.chdir('/opt/django')
@@ -20,22 +20,22 @@ def install_django():
     os.system('source bin/activate ' + \
               'django-admin startproject project1')
     os.system('chown -R centos /opt/django')
-    
+
 def start_django():
-    
-    os.system('sudo -u centos && ' + \
+
+    os.system('sudo -u centos ' + \
              'source /opt/django/bin/activate &&' + \
              '/opt/django/project1/manage.py runserver 0:8000 &')
-    
 
-print('Setting up the install')
-input('Press any key to continue')
+
+print('***************Setting up the install**********************')
+#raw_input('Press any key to continue')
 setup_install()
 
-print('Installing Django')
-input('Press any key to continue')
+print('******************Installing Django************************')
+#raw_input('Press any key to continue')
 install_django()
 
-print('Starting the server')
-input('Press any key to continue')
+print('****************Starting the server****************************')
+#raw_input('Press any key to continue')
 start_django()
