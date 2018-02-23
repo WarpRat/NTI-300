a65ace31b800fad7038dd211102edd4a34bd83e7
Robert Russell <23439640+WarpRat@users.noreply.github.com>
2018-02-22 20:37:45 -0800

    Python version of the django install script

---
+++ install_django.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import os
+
+def setup_install():
+    
+    os.system('yum install -y epel-release')
+    os.system('yum update -y')
+    os.system('yum install -y python34 python-pip')
+    os.system('pip install --upgrade pip')
+    os.system('pip install django')
+    
+def install_django():
+    
+    os.chdir('/opt')
+    os.system('virtualenv -p python3 django')
+    os.chdir('/opt/django')
+    os.system('source bin/activate ' + \
+             '&& pip install django')
+    os.system('source bin/activate ' + \
+              'django-admin startproject project1')
+    os.system('chown -R centos /opt/django')
+    
+def start_django():
+    
+    os.system('sudo -u centos && ' + \
+             'source /opt/django/bin/activate &&' + \
+             '/opt/django/project1/manage.py runserver 0:8000 &')
+    
