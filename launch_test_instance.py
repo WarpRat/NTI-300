ecf72cca7616910447749ccee8acbf7d1939175e
Robert Russell <23439640+WarpRat@users.noreply.github.com>
2018-03-02 21:01:41 -0800

    Commented version of Nicole's code. Doesn't run. Needs AWS keys and other defaults set

---
+++ launch_test_instance.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+
+#################
+##
+## Robert Russell
+## 3/2/18
+## NTI-300 commented code
+## cloned from https://github.com/nic-instruction/NTI-300-2017.git
+##
+#################
+
+#Importing modules
+
+import boto3   #Imports the python module that can be used to control AWS resources
+import base64  #Used to convert strings to and from base64 encoding. I don't actually see it used here however. Could be used to convert userdata to a base64 string.
+import pprint  #Python's pretty print module. Easy way to display API responses and other things in human readable form.
+
+#Creating variables to make using the boto3 module easier. A more readable option than importing python modules using the form `from boto3 import resource as br`.
+
+ec2 = boto3.resource('ec2')  #Create a shortcut variable for the boto3 ec2 resource type.
+client = boto3.client('ec2')  #Create a shortcut variable for the boto3 ec2 client.
+
+
+#Creating variables here to be used later in creating the EC2 instance.
+
+amazon_image = 'ami-392ba941'   #Will install the CentOS AMI I created based on the official CentOS cloud image with updated storage. 
+amazon_instance = 't2.micro'    #Instance size                                  
+amazon_pem_key = 'django_dev.pem' #My private key. Actually created months ago last time I was playing with django.
+firewall_profiles = ['launch-wizard-4']   #The security group I've been using with the following ports open: 22,80,443,8000.
+
+
+#Print the current variables to confirm to the user that they are correct or to be logged for troubleshooting.
+
+print(amazon_image)
+print(amazon_instance)
+print(amazon_pem_key)
+
+#Define a module to launch instances.
+
+def launch_test_instance():
+
+   instances = ec2.create_instances(   #Create a variable that stores the response to creating the instance with the previous boto3 shortcut variable.
+      ImageId = amazon_image,          #References the image from above.
+      InstanceType = amazon_instance,  #References the image type from above.
+      MinCount=1,                      #Autoscaling options.
+      MaxCount=1,                      #Autoscaling options.
+      KeyName = amazon_pem_key,        #Name of the keypair you want to use to authenticate.
+      SecurityGroupIds = firewall_profiles,   #Name of the security group to attach.
+      UserData="""#!/usr/bin/python          #All of this commented out section is the userdata script that runs on first launch.
+import sys, os                               #This particular script would install git and clone a public git repo and then use python
+def set_up_git():                            #to install apache.
+   print('install git')
+   os.system('yum -y install git')
+   
+   print('installing my reposoitory')
+   os.system('git clone https://github.com/nic-instruction/python_deploy.git /tmp/python_deploy')
+
+set_up_git()
+
+sys.path.append('/tmp/python_deploy')
+
+import app_install
+
+app_install.install_apache()
+
+"""
+
+    )
+
+   pprint.pprint(instances)   #This will print the response returned from boto3 command.
+
+
+launch_test_instance()  #This calls the function created above. It will require that the machine it's being run from holds the proper IAM role to launch EC2 instances.
+
